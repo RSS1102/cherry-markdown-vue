cb99d5950641cec72c48da8d4220f78d26c0d2f3
任帅 <1064425721@qq.com>
2024-06-17 13:48:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CherryMarkdown from 'cherry-markdown'
+import { ref, shallowRef } from 'vue';
+const cherry = shallowRef();
+cherry.value = new CherryMarkdown({
+  id: 'cherry-markdown',
+  value: '# test CherryMarkdown',
+})
+
+const setValue = () => {
+  cherry.value.setValue('# Hello CherryMarkdown')
+}
+
+</script>
+
+<template>
+  <div id="cherry-markdown" />
+  <button @click="setValue">Set Value</button>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'cherry-markdown/dist/cherry-markdown.css'
+
+createApp(App).mount('#app')
